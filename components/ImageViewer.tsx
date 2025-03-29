e25eb6dd093698b945e5b80543c880a2f1758cbc
Kamil Talanda <kamil0916@gmail.com>
2025-03-29 22:00:35 +0100

    Pick image

--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -3,10 +3,12 @@ import { Image, type ImageSource } from "expo-image";
 
 type Props = {
   imgSource: ImageSource;
+  selectedImage?: string;
 };
 
-export default function ImageViewer({ imgSource }: Props) {
-  return <Image style={styles.image} source={imgSource} />;
+export default function ImageViewer({ imgSource, selectedImage }: Props) {
+  const imageSource = selectedImage ? { uri: selectedImage } : imgSource;
+  return <Image style={styles.image} source={imageSource} />;
 }
 
 const styles = StyleSheet.create({
