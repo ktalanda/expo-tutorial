34a889c87be83bc1151e1a22e78a9422db000f8a
Kamil Talanda <kamil0916@gmail.com>
2025-04-04 21:42:51 +0200

    Save screenshot in web

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,10 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Platform } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import * as MediaLibrary from "expo-media-library";
 import { useState, useRef } from "react";
 import { captureRef } from "react-native-view-shot";
 import { type ImageSource } from "expo-image";
+import domtoimage from "dom-to-image";
 
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
@@ -58,17 +59,36 @@ export default function Index() {
   };
 
   const onSaveImage = async () => {
-    try {
-      const localUri = await captureRef(imageRef, {
-        height: 440,
-        quality: 1,
-      });
-      await MediaLibrary.saveToLibraryAsync(localUri);
-      if (localUri) {
-        alert("Image saved to gallery!");
+    if (Platform.OS !== "web") {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert("Image saved to gallery!");
+        }
+      } catch (error) {
+        console.error("Error saving image: ", error);
       }
-    } catch (error) {
-      console.error("Error saving image: ", error);
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        let link = document.createElement("a");
+        link.download = 'sticker-smash.jpeg';
+        link.href = dataUrl;
+        link.click();
+
+      }
+      catch (error) {
+        console.error("Error saving image: ", error);
+      }
     }
   }
 
